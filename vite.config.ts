6c97b42ce408a4006cf76e26014e6795d03a3bfb
Seyed Amin Hosseini <sydaminhosseini@gmail.com>
2026-08-17 18:35:45 +0330

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/amin-hosseini-portfolio/",
-  plugins: [react()],
-});
+export default defineConfig({ plugins: [react()] });
